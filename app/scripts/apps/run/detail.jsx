f03409bcc3e1b86e8cb771518974aff831a8044d
Timo Behrmann <timo.behrmann@gmail.com>
2015-03-10 18:01:35 +0100

    no message

--- app/scripts/apps/run/detail.jsx
+++ app/scripts/apps/run/detail.jsx
@@ -8,9 +8,33 @@ var Reflux = require('reflux');
 var Actions = require('../../actions/runActions');
 var Stores = require('../../stores/runStores');
 
+var SetIntervalMixin = {
+    setInterval: function() {
+        this.clearInterval();
+        this.interval = (setInterval.apply(null, arguments));
+    },
+    clearInterval: function() {
+        if (this.interval) {
+            clearInterval(this.interval)
+            this.interval = null;
+        }
+    },
+    componentWillUnmount: function() {
+        this.clearInterval();
+    }
+};
+
 module.exports = React.createClass({
-    mixins: [Router.Navigation, Router.State, Reflux.connect(Stores.Get), Reflux.ListenerMixin],
+    mixins: [Router.Navigation, Router.State, Reflux.connect(Stores.Get), Reflux.ListenerMixin, SetIntervalMixin],
     componentDidMount: function() {
+        this.load();
+    },
+    startRefreshing: function() {
+        if (!this.interval) {
+            this.setInterval(this.load, 1000);
+        }
+    },
+    load: function() {
         var params = this.getParams();
         Actions.get(params.id);
     },
@@ -27,7 +51,14 @@ module.exports = React.createClass({
             return memo;
         }, []);
 
-        var exit = this.state.exitCode ? <p className="exit" data-code={this.state.exitCode}><a/><span>Done. Process exited with exit code {this.state.exitCode}.</span></p> : null;
+        var processExited = this.state.exitCode !== undefined;
+        var exit = processExited ? <p className="exit" data-code={this.state.exitCode}><a/><span>Done. Process exited with exit code {this.state.exitCode}.</span></p> : null;
+
+        if (!processExited) {
+            this.startRefreshing();
+        } else {
+            this.clearInterval();
+        }
 
         return (
             <div className="page-main">
